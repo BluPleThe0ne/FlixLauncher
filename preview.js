3b579d50c3c942f1dcec23f382b324df05dbe9a0
BluPleThe0ne <136716134+BluPleThe0ne@users.noreply.github.com>
2024-01-13 18:11:32 +0100

    Add files via upload

---
+++ preview.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("appslist").style.visibility = "hidden"; 
+    document.getElementById("topcntrler").style.visibility = "hidden"; 
+    document.getElementById("toast").style.visibility = "hidden";
+});
